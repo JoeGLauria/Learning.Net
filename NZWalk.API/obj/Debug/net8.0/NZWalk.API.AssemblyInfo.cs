9ccb34477d1216e5f5778675c4add23a98d20af8
JoeGLauria <129402373+JoeGLauria@users.noreply.github.com>
2024-12-02 15:08:04 +0000

    add delete region action method

--- NZWalk.API/obj/Debug/net8.0/NZWalk.API.AssemblyInfo.cs
+++ NZWalk.API/obj/Debug/net8.0/NZWalk.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NZWalk.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+337909cba8167f2bac8129be7dcae8f2b2a17615")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5a5dbcf96e3e8960d55478e099b3340e89555ba5")]
 [assembly: System.Reflection.AssemblyProductAttribute("NZWalk.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NZWalk.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
